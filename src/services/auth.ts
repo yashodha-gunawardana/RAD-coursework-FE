ebb6afc3a8e47c37ef4c2aaf695d5d166396047a
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 15:10:36 +0530

    create register function to call backend API

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,16 @@
 // This allows us to send HTTP requests to the backend easily
 import api from "./api";
 
-
+// defines the shape of data the frontend sends to the backend when registering
 type RegisterDataType = {
     fullname: string
     email: string
     password: string
     address?: string
     phone?: string
+}
+
+// register function
+export const registerUser = async (data: RegisterDataType) => {
+
 }
