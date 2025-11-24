ac3844c564d8c606ab514d8f643d5ce2bcc6c0b9
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 20:00:56 +0530

    allows page redirect programmatically

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,3 +2,7 @@ import { useState, type FormEvent } from "react";
 import { getMyDetails, loginUser } from "../services/auth";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
+
+export default function Login() {
+    const navigate = useNavigate()
+}
