d5d95ef3b1073414c515aee26db302aa3017a546
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 17:46:12 +0530

    AuthProvider wraps the whole app and provides auth data

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,3 +1,9 @@
-import { createContext } from "react";
+import { Children, createContext } from "react";
 
+// AuthContext will store auth data (user, setUser, loading)
 const AuthContext = createContext<any>(null)
+
+// AuthProvider wraps the whole app and provides auth data
+export const AuthProvider = ({ Children }: any) => {
+
+}
