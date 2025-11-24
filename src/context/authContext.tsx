0b6f2888aefcb5bb3b70af46e4892452ec937d42
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 19:43:26 +0530

    safety check to ensure Provider is used

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -53,4 +53,8 @@ export const AuthProvider = ({ children }: any) => {
 export const useAuth = () => {
     const context = useContext(AuthContext)
 
+    if (!context) {
+        throw  new Error("useAuth must be used within an AuthProvider")
+    }
+
 }
