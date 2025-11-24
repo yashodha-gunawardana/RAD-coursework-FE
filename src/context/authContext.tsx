529aa1bb0eac814eaa62e9bcbb6abb7601db01d8
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 17:49:36 +0530

    use to store user and loading states

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,9 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // AuthContext will store auth data (user, setUser, loading)
 const AuthContext = createContext<any>(null)
 
 // AuthProvider wraps the whole app and provides auth data
 export const AuthProvider = ({ children }: any) => {
+    // store logged-in user details(null means not logged in)
     const [user, setUser] = useState<any>(null)
 }
