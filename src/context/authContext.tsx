31c77fa0a6a620e9cea4c8b85269892c30bdcc46
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 17:48:18 +0530

    store logged in-user details

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,9 +1,9 @@
-import { Children, createContext } from "react";
+import { createContext } from "react";
 
 // AuthContext will store auth data (user, setUser, loading)
 const AuthContext = createContext<any>(null)
 
 // AuthProvider wraps the whole app and provides auth data
-export const AuthProvider = ({ Children }: any) => {
-
+export const AuthProvider = ({ children }: any) => {
+    const [user, setUser] = useState<any>(null)
 }
