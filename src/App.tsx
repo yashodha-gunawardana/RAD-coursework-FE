2b635f8ca4f4bdd1bbe3069c4735a215120a9cb6
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 23:16:40 +0530

    this allows any child component to access user/login data using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 // stores logged-in user data, authenticate status, users info with all pages
 import { AuthProvider } from "./context/authContext";
 
+
+// define the main App component
 export default function App() {
+  return (
+    //This allows any child component to access user/login data using context
+    <AuthProvider>
+      
+      {/* Router component handles page navigation */}
+      <Router />
+
+    </AuthProvider>
+  )
   
 }  
