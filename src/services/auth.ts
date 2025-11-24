71cf69b8d6416e6f435bf7836a6ee5cbcc1b55f3
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 15:05:17 +0530

    defines the shape of data the frontend sends to the backend when registering

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,1 +1,11 @@
+// This allows us to send HTTP requests to the backend easily
 import api from "./api";
+
+
+type RegisterDataType = {
+    fullname: string
+    email: string
+    password: string
+    address?: string
+    phone?: string
+}
