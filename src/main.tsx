b5822b529101e86c2c50415305b94da3c1ecb9ba
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-24 23:45:10 +0530

    create a react root and attach it to the HTML element with id 'root"

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
 import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx'
+import './App.css'
+
+// The '!' tells TypeScript that this element exists and is not null
+createRoot(document.getElementById('root')!).render(
+
+)
