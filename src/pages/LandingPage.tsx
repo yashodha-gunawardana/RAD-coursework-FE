c0889386ed045e69973e17f0531900fba9924813
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-27 16:41:23 +0530

    added array of hero slider images

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { Link } from  "react-router-dom";
+import { useEffec, useState } from "react"
 
 
 export default function LandingPage() {
+    const images = [
+        "https://images.unsplash.com/photo-1519225421980-715cb0215aed?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1515934751635-c81c6ebb6c1a?q=80&w=2940&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1464366400609708-7b9f2e6c07.jpg?q=80&w=2940&auto=format&fit=crop",
+    ]
+    
     return (
         <div>
-            
+
         </div>
     )
 }
