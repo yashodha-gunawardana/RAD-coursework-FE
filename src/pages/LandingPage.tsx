664a05226e87058d78a18dcb703a7536b30d2cea
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-11-27 16:53:59 +0530

    if the new index is greater than or equal to the number of slides, wrap around to the first slide

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -13,6 +13,7 @@ export default function LandingPage() {
     const changeSlide = (direction: number) => {
         let newIndex = currentIndex + direction;
         if (newIndex < 0) newIndex = images.length - 1;
+        if (newIndex >= images.length) newIndex = 0;
     }
     return (
         <div>
