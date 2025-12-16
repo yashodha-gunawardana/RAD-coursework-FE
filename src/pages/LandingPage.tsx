78e9b84e1bbd92575432feabd4176176cf633957
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-16 22:11:14 +0530

    vertical divider hidden on mobile for space

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -489,6 +489,8 @@ export default function LandingPage() {
 
                                             </div>
                                         </div>
+
+                                        <div className="w-px h-8 bg-[#E8E3D8] hidden md:block"></div>
                                     </div>
                                 </div>
                             </div>
