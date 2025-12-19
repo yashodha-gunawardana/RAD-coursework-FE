9fcd22b1e4805a0b82897d56620ffecfadd55f5d
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-19 19:18:43 +0530

    create ServiceSection component

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -2,9 +2,15 @@ import React, { useState, useRef, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'react-feather';
 import '../App.css';
 
+
 interface service {
   id: number
   name: string
   price: string
   img: string
+}
+
+
+const ServiceSection: React.FC = () => {
+  
 }
