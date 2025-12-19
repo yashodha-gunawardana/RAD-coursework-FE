dfafc6031a64bac84388db80cf28cd3f7d96e640
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-19 19:32:32 +0530

    define Service interface for type safety

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -1,3 +1,10 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'react-feather';
 import '../App.css';
+
+interface Service {
+  id: number;
+  name: string;
+  price: string;
+  img: string;
+}
