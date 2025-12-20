ba497dd39c09ebd7f5198a41418dbc64e7444154
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-20 19:56:47 +0530

    full background design layer

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -6,6 +6,10 @@ const StatsSection: React.FC = () => {
     return (
         <div className="relative py-16 px-4 md:px-8 bg-[#0F0F0F]/80 overflow-hidden font-[Poppins, serif] min-h-[200px]">
 
+            {/* background */}
+            <div className="absolute inset-0 z-0">
+
+            </div>
         </div>
     )
 }
