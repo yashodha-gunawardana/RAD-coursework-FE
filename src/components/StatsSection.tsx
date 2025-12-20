64288fb0a913454e8fd9ab6d0f1828410d4467c2
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-20 19:55:39 +0530

    create a row section

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -2,5 +2,12 @@ import React from "react";
 
 
 const StatsSection: React.FC = () => {
-    
+
+    return (
+        <div className="relative py-16 px-4 md:px-8 bg-[#0F0F0F]/80 overflow-hidden font-[Poppins, serif] min-h-[200px]">
+
+        </div>
+    )
 }
+
+export default StatsSection
