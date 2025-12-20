ab16676f1b96b1a90da531c9a14df3417b17d315
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-20 23:10:50 +0530

    main radial gradient

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -4,11 +4,15 @@ import React from "react";
 const StatsSection: React.FC = () => {
 
     return (
-        <div className="relative py-16 px-4 md:px-8 bg-[#0F0F0F]/80 overflow-hidden font-[Poppins, serif] min-h-[200px]">
+        <div className="relative py-16 px-4 md:px-8 bg-[#0F0F0F]/80 overflow-hidden font-[Poppins, serif] min-h-[250px]">
 
             {/* background */}
             <div className="absolute inset-0 z-0">
+                <div className="absolute inset-0 bg-[radial-gradient(circle_at_80%_20%,rgba(195,153,83,0.1)_0%,transparent_50%]"></div>
 
+                <div >
+
+                </div>
             </div>
         </div>
     )
