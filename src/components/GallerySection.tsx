34e1f26e9f533d91e24a61f476f6274d233cafca
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-23 11:31:24 +0530

    structure of a single gallery item

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 import { Layers, PlayCircle, Camera, Search } from "react-feather";
 
 
+// type defenitions
 type GalleryCategory = "photo" | "video"
 type FilterType = "all" | GalleryCategory
+
+
+interface GalleryItemType {
+    id: number
+    category: GalleryCategory
+    image: string
+    title: string
+    date: string
+}
