d99d9a18bfb5bc4f70cff478de09ac510ac082a7
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-24 15:37:28 +0530

    floating diamond element for background decoration

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -27,6 +27,11 @@ const ContactSection = () => {
         <section id="conatct" className="relative py-24 px-5 bg-[#FDFCFB] font-[Poppins] overflow-hidden
                                             flex justify-center">
 
+            <div className="absolute w-72 h-72 border border-[#E5DED1] top-10 left-5 rotate-45 z-0 opacity-50 
+                            animate-rotateDiamond">
+
+            </div>
+            
         </section>
     )
 }
