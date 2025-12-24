0a9c680ac04cfab1fc1655b2040edb0f1d6b7777
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-24 15:08:58 +0530

    state to manage form inputs

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,5 +2,10 @@ import React, { useState } from "react";
 
 
 const ContactSection = () => {
-    
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        eventType: "",
+        message: ""
+    })
 }
