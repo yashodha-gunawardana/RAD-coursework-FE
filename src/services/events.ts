f2f6d4e5568cc53341a5a9938f4fa50c15bf2506
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-29 12:23:02 +0530

    create new event

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,2 +1,11 @@
 import api from "./api";
 
+
+export const createEvent = async (data: any) => {
+    const res = await api.post("/events", data, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    });
+    return res.data
+}
