facee61fe5bae7a42ab9aaaedc038ec44f51d6fb
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-29 12:25:04 +0530

    update, delete, get events

--- src/services/events.ts
+++ src/services/events.ts
@@ -8,4 +8,31 @@ export const createEvent = async (data: any) => {
         }
     });
     return res.data
-}
+};
+
+export const getMyEvents = async () => {
+  const res = await api.get("/events");
+  return res.data;
+};
+
+
+export const getEventById = async (id: string) => {
+  const res = await api.get(`/events/${id}`);
+  return res.data;
+};
+
+
+export const updateEvent = async (id: string, data: any) => {
+  const res = await api.put(`/events/${id}`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
+  return res.data;
+};
+
+
+export const deleteEvent = async (id: string) => {
+  const res = await api.delete(`/events/${id}`);
+  return res.data;
+};
