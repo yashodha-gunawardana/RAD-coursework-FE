2b67d277085e54bfa0d2156a3d6eb1f15ec0e25e
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-12-30 19:29:41 +0530

    update, delete get vendors api

--- src/services/vendor.ts
+++ src/services/vendor.ts
@@ -1,11 +1,39 @@
 import api from "./api";
 
-// create vendor
+// create vendor (admin)
 export const createVendor = async (data: FormData) => {
     const res = await api.post("/vendors", data, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
     })
+    return res.data
+}
+
+// get all vendors (public)
+export const getAllVendors = async () => {
+    const res = await api.get("/vendors")
+    return res.data
+}
+
+// get vendor by id (public)
+export const getvendorById = async (id: string) => {
+    const res = await api.get(`/vendors/${id}`)
+    return res.data
+}
+
+// update vendor (admin)
+export const updateVendor = async (id: string, data: FormData) => {
+    const res = await api.put(`/vendors/${id}`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+    return res.data
+}
+
+// delete vendor (admin)
+export const deleteVendor = async (id: string) => {
+    const res = await api.delete(`/vendors/${id}`)
     return res.data
 }
