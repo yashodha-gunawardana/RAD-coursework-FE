b68032ab7d173c0ef6cbcc13561e60fe4c71ada9
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-02 16:36:30 +0530

    match backend model exactly

--- src/pages/dashboard/UsersPage.tsx
+++ src/pages/dashboard/UsersPage.tsx
@@ -13,3 +13,18 @@ import {
   Clock,
 } from "react-feather";
 import { getAllUsers, approveVendorRequest, rejectVendorRequest } from "../../services/auth";
+
+
+export const Role = {
+    ADMIN: "ADMIN",
+    VENDOR: "VENDOR",
+    USER: "USER"
+} as const
+
+
+export const vendorStatus = {
+    NOT_REQUESTED: "NOT_REQUESTED",
+    PENDING: "PENDING",
+    APPROVED: "APPROVED",
+    REJECTED: "REJECTED"
+} as const
