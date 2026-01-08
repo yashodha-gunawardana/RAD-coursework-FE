e6d4ac8dc222a3d217b08240cdadb91e903c76fa
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-08 23:02:21 +0530

    return AI response or fallback message

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "http://localhost:5000/api/v1/ai"; 
 
 
-export const getAIRecommendation = async (eventType: string, prompt: string) = > {
+export const getAIRecommendation = async (eventType: string, prompt: string) => {
     try {
 
         const token = localStorage.getItem("token")
@@ -15,9 +15,12 @@ export const getAIRecommendation = async (eventType: string, prompt: string) = >
                 eventName: prompt,
                 question: prompt
             },
-            { headers: { Authorization: `Bearer ${token}`} }
+            { headers: { Authorization: `Bearer ${token}` } }
         )
-    } catch (err) {
+        return response.data.aiResponse || Response.data.message
 
+    } catch (err) {
+        console.log("AI service error: ", err)
+        return "Sorry, I couldn't process your request, Please try again"
     }
 }
