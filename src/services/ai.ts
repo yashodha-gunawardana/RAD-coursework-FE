53f610b0a2138de93134fa7656373ce7a12f4986
yashodha-25 <yashodagunawardhana15@gmail.com>
2026-01-08 22:58:44 +0530

    sen post request to AI endpoint

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -6,6 +6,17 @@ const API_URL = "http://localhost:5000/api/v1/ai";
 export const getAIRecommendation = async (eventType: string, prompt: string) = > {
     try {
 
+        const token = localStorage.getItem("token")
+
+        const response = await axios.post(
+            `${API_URL}/event-consultant`,
+            {
+                eventType,
+                eventName: prompt,
+                question: prompt
+            },
+            { headers: { Authorization: `Bearer ${token}`} }
+        )
     } catch (err) {
 
     }
